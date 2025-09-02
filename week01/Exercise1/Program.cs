6ef77ca521373bd6555c697594c69ab6f008d5b9
Emmanuel Uko <ojongjr@outlook.com>
2025-09-02 20:16:34 +0100

    Added the name display function

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        String firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        String lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your Name is {lastName}, {firstName} {lastName}.");
+
+
+        // Adding a fun element
+        String name = $"{firstName} {lastName}";
+
+        Console.WriteLine($"Welcome, {name}!");
+
+        Console.Write("Password? ");
+        String password = Console.ReadLine();
+        
+        if (password == "BOND007")
+        {
+            Console.WriteLine("Access Granted");
+        }
+        else
+        {
+            Console.WriteLine("Access Denied");
+        }
     }
 }
