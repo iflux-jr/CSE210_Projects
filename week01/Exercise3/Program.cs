c94b5d2b51987ae9ff980556b72940982327a3f8
Emmanuel Uko <ojongjr@outlook.com>
2025-09-03 10:11:46 +0100

    Added the 'Guess My Number' game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        String playAgain;
+
+        do
+        {
+            // Console.WriteLine("What is the magic number? ");
+            // String magicInput = Console.ReadLine();
+            // int magicNumber = int.Parse(magicInput);
+
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int guessCount = 0;
+            String guessInput;
+
+            Console.WriteLine("I have picked a number between 1 and 100.");
+            Console.WriteLine("Can you guess the magic number?");
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                guessInput = Console.ReadLine();
+                guess = int.Parse(guessInput);
+
+                // Guess count
+                guessCount++;
+
+                // Loop
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {guessCount} tries!");
+                }
+            } while (guess != magicNumber);
+
+            // Ask user if they want to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower();
+
+        } while (playAgain == "yes");
+
+        Console.WriteLine("Thanks for playing! Goodbye.");
+
     }
 }
